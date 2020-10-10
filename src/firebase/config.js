2e7c078bd4c1b2ca44eabe35a4db0af764c2e062
annguyen2790 <tuh09895@temple.edu>
2020-10-10 17:53:59 -0400

    Added API keys and can be run and test on mobile device

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
+  apiKey: 'AIzaSyCPSeidZCEHWXsA7IsztUiB4_idum27lC8',
+  authDomain: 'https://fir-react-native-b2fff.firebaseio.com',
+  databaseURL: 'https://fir-react-native-b2fff.firebaseio.com',
+  projectId: 'fir-react-native-b2fff',
+  storageBucket: 'fir-react-native-b2fff.appspot.com',
   messagingSenderId: '',
-  appId: '',
+  appId: '1:491143214913:android:1ace677f9994e86fcc13c0',
 };
 
 if (!firebase.apps.length) {
