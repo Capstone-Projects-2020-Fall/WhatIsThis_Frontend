69228d03e34d539054520cc81f41b83f32d96740
Parth Patel <tug59168@temple.edu>
2020-10-30 22:07:52 -0400

    Update MuscleSelectorScreen.js

--- screens/MuscleSelectorScreen.js
+++ screens/MuscleSelectorScreen.js
@@ -1,10 +1,10 @@
 // Aboutscreen.js
 import React, { Component } from 'react';
-import { Button, View, Text, Alert } from 'react-native';
+import { Button, View, Text, Alert, Image, Modal, TouchableOpacity, StyleSheet } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import {firestore} from 'firebase';
 import {testReturn, getExerciseArrayFromFirestore, returnExerciseList} from '../helpers';
-
+/*
 //import {workoutInfoByMachine,workoutInfoByMuscle} from '../helpers';
 export default class MuscleSelectorScreen extends Component {
   constructor(props){
@@ -29,7 +29,110 @@ export default class MuscleSelectorScreen extends Component {
       </View>
     )
   }
-}
+}*/
 
 
+export default class MuscleSelectorScreen extends Component {
+  // initial state
+  state = {
+    isVisible: false
+  };
 
+  // hide show modal
+  displayModal(show){
+    this.setState({isVisible: show})
+  }
+
+  render() {
+    return (
+      <View style = { styles.container }>
+        <Modal
+            animationType = {"slide"}
+            transparent={false}
+            visible={this.state.isVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has now been closed.');
+            }}>
+
+            <Image 
+           
+              style = { styles.image }/>
+
+
+              <Text 
+                style={styles.text}
+                onPress={() => {
+                  this.displayModal(!this.state.isVisible);}}>Bicep Curl</Text>
+          </Modal>
+            
+          <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                this.displayModal(true);
+              }}>
+              <Text style={styles.buttonText}>Biceps</Text>
+          </TouchableOpacity>          
+        </View>
+      );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 25,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    display: 'flex',
+    height: 60,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#2AC062',
+    shadowColor: '#2AC062',
+    shadowOpacity: 0.5,
+    shadowOffset: { 
+      height: 10, 
+      width: 0 
+    },
+    shadowRadius: 25,
+  },
+  closeButton: {
+    display: 'flex',
+    height: 60,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF3974',
+    shadowColor: '#2AC062',
+    shadowOpacity: 0.5,
+    shadowOffset: { 
+      height: 10, 
+      width: 0 
+    },
+    shadowRadius: 25,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 22,
+  },
+  image: {
+    marginTop: 150,
+    marginBottom: 10,
+    width: '100%',
+    height: 350,
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 30,
+    padding: 40,
+  },
+  closeText: {
+    fontSize: 24,
+    color: '#00479e',
+    textAlign: 'center',
+  }
+});
