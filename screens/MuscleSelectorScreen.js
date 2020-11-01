45e42bb27dc22dffa430c3fdebd316d133ac2c8a
Parth Patel <tug59168@temple.edu>
2020-10-31 21:15:02 -0400

    Exercise name and descriptions to appear on app

--- screens/MuscleSelectorScreen.js
+++ screens/MuscleSelectorScreen.js
@@ -1,9 +1,10 @@
 // Aboutscreen.js
 import React, { Component } from 'react';
-import { Button, View, Text, Alert, Image, Modal, TouchableOpacity, StyleSheet } from 'react-native';
+import { Button, View, Text, Alert, Image, Modal, TouchableOpacity, StyleSheet, TouchableHighlight, TouchableWithoutFeedback, ScrollView } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import {firestore} from 'firebase';
 import {testReturn, getExerciseArrayByMuscle, getExerciseArrayFromFirestore, returnExerciseList, returnMuscleExerciseList} from '../helpers';
+
 /*
 //import {workoutInfoByMachine,workoutInfoByMuscle} from '../helpers';
 export default class MuscleSelectorScreen extends Component {
@@ -55,41 +56,75 @@ class MuscleSelectorScreen extends Component {
 						this.setState({exercises: data});
 					});
   }
-
-  
-  
   
 
   render() {
 	  const {exercises} = this.state;
 		//console.log(exercises[0]);
 		const exerciseNames = new Array();
-		
+    const exerciseDes = new Array();
+    const exerciseMuscle = new Array();
+    const exerciseMachine = new Array();
+
+    const muscleExerciseList = new Array();
 		exercises.forEach(exercise => {
-			exerciseNames.push(exercise.name);
-		});
-		
+      exerciseNames.push(exercise.name);
+      exerciseDes.push(exercise.description);
+      //exerciseMuscle.push(exercise.muscle[2]);
+      //exerciseMachine.push(exercise.machine);
+    });
+    
+    function buildArray(muscleID){
+      exercises.forEach(exercise => {
+        if(exercise.muscle == muscleID){
+            muscleExerciseList.push(exercise.name, "\n\n",exercise.description, "\n\n\n");
+          
+        }
+      })
+      return muscleExerciseList;
+    }
+    
+    /**
+    var res = [];
+    map.forEach(function(val, key) {
+      res.push({ region: key, value: val });
+    });
+     */
 		
     return (
       <View style = { styles.container }>
         <Modal
             animationType = {"slide"}
-            transparent={false}
+            transparent={true}
             visible={this.state.isVisible}
             onRequestClose={() => {
-              Alert.alert('Modal has now been closed.');
-            }}>
-
-            <Image 
-           
-              style = { styles.image }/>
-
-
-              <Text 
-                style={styles.text}
-                onPress={() => {
-					console.log(exercises[0]);
-                  }}>{exerciseNames}</Text>
+              //Alert.alert('Modal has now been closed.');
+              //onRequestClose={() => {this.setModalVisible(false)}}
+              {this.setModalVisible(false)
+            }}
+            }          
+            >
+            <TouchableOpacity 
+            style={styles.container} 
+            activeOpacity={1} 
+            onPressOut={() => {this.displayModal(false)}}
+          >
+            
+            <TouchableWithoutFeedback>
+                
+                <View style={styles.modalView}>
+                <Text 
+                style={styles.modelText}
+                
+              >
+                
+                {buildArray('biceps brachii')}
+                
+              </Text>
+                </View>
+              
+              </TouchableWithoutFeedback>
+            </TouchableOpacity>
           </Modal>
             
           <TouchableOpacity
@@ -100,6 +135,7 @@ class MuscleSelectorScreen extends Component {
               <Text style={styles.buttonText}>Biceps</Text>
           </TouchableOpacity>          
         </View>
+        
       );
   }
 };
@@ -157,11 +193,34 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     padding: 40,
   },
+  modelText: {
+    fontSize: 14,
+    marginBottom: 10,
+    padding: 1,
+  },
   closeText: {
     fontSize: 24,
     color: '#00479e',
     textAlign: 'center',
-  }
+  },
+  modalView: {
+    margin: 5,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    }
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
 });
 
 export default MuscleSelectorScreen;
