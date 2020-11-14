d0e3206ea2e6301ddd3d7dfead74498456d7e52b
Parth Patel <tug59168@temple.edu>
2020-11-14 18:58:46 -0500

    fixed the bug for depreciation warning

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,113 +1,272 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
-import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import {Agenda} from 'react-native-calendars';
+import {
+  Platform,
+  Alert,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Button
+} from 'react-native';
+import {ExpandableCalendar, AgendaList, CalendarProvider, WeekCalendar} from 'react-native-calendars';
 
 
 
 
-export default class AgendaScreen extends Component {
-  constructor(props) {
-    super(props);
+const today = new Date().toISOString().split('T')[0];
+const fastDate = getPastDate(3);
+const futureDates = getFutureDates(9);
+const dates = [fastDate, today].concat(futureDates);
+const themeColor = '#00AAAF';
+const lightThemeColor = '#EBF9F9';
 
-    this.state = {
-      items: {}
+function getFutureDates(days) {
+  const array = [];
+  for (let index = 1; index <= days; index++) {
+    const date = new Date(Date.now() + (864e5 * index)); // 864e5 == 86400000 == 24*60*60*1000
+    const dateString = date.toISOString().split('T')[0];
+    array.push(dateString);
+  }
+  return array;
+}
+
+function getPastDate(days) {
+  return new Date(Date.now() - (864e5 * days)).toISOString().split('T')[0];
+}
+
+// Pass in the date, something like 11/24/2020 or whatever format that works
+// then return the index number for that day for the date array.
+/*function countDates(days){
+  
+  return indexDay;
+}*/
+
+
+
+/**
+ * The ITEMS array holds the events
+*/
+const ITEMS = [
+  {title: dates[0], data: [{hour: '12am', duration: '1h', title: 'First Yoga'}]},
+  {title: dates[1], data: [{hour: '4pm', duration: '1h', title: 'Pilates ABC'}, {hour: '5pm', duration: '1h', title: 'Vinyasa Yoga'}]},
+  {title: dates[2], data: [{hour: '1pm', duration: '1h', title: 'Ashtanga Yoga'}, {hour: '2pm', duration: '1h', title: 'Deep Streches'}, {hour: '3pm', duration: '1h', title: 'Private Yoga'}]},
+  {title: dates[3], data: [{hour: '12am', duration: '1h', title: 'Ashtanga Yoga'}]},
+  {title: dates[4], data: [{hour: '6pm', duration:'1h', title: "Running"}]},
+  {title: dates[5], data: [{hour: '9pm', duration: '1h', title: 'Middle Yoga'}, {hour: '10pm', duration: '1h', title: 'Ashtanga'}, {hour: '11pm', duration: '1h', title: 'TRX'}, {hour: '12pm', duration: '1h', title: 'Running Group'}]},
+  {title: dates[6], data: [{hour: '12am', duration: '1h', title: 'Ashtanga Yoga'}]},
+  //{title: dates[7], data: [{}]},
+  {title: dates[8], data: [{hour: '9pm', duration: '1h', title: 'Pilates Reformer'}, {hour: '10pm', duration: '1h', title: 'Ashtanga'}, {hour: '11pm', duration: '1h', title: 'TRX'}, {hour: '12pm', duration: '1h', title: 'Running Group'}]},
+  {title: dates[9], data: [{hour: '1pm', duration: '1h', title: 'Ashtanga Yoga'}, {hour: '2pm', duration: '1h', title: 'Deep Streches'}, {hour: '3pm', duration: '1h', title: 'Private Yoga'}]},
+  {title: dates[10], data: [{hour: '12am', duration: '1h', title: 'Last Yoga'}]}
+];
+
+
+const eventArrExercise = ITEMS.map(ITEMS => ITEMS.title, ITEMS.data);
+console.log(eventArrExercise);
+
+function addEvent(){
+  eventExercise = {title: dates[21], data: [{hour: hour, duration: duration, title: title}]}
+  hourTime='2pm'
+  duraction='1h'
+  title='Dumbells'
+  ITEMS.push(eventExercise);
+  return ITEMS;
+}
+
+
+export default class ExpandableCalendarScreen extends Component {
+
+  onDateChanged = (/* date, updateSource */) => {
+    // console.warn('ExpandableCalendarScreen onDateChanged: ', date, updateSource);
+    // fetch and set data for date + week ahead
+  }
+
+  onMonthChange = (/* month, updateSource */) => {
+    // console.warn('ExpandableCalendarScreen onMonthChange: ', month, updateSource);
+  }
+
+  buttonPressed() {
+    Alert.alert('show more');
+  }
+
+  itemPressed(id) {
+    Alert.alert(id);
+  }
+
+  renderEmptyItem() {
+    return (
+      <View style={styles.emptyItem}>
+        <Text style={styles.emptyItemText}>No Events Planned</Text>
+      </View>
+    );
+  }
+
+  renderItem = ({item}) => {
+    if (_.isEmpty(item)) {
+      return this.renderEmptyItem();
+    }
+
+    return (
+      <TouchableOpacity
+        onPress={() => this.itemPressed(item.title)}
+        style={styles.item}
+        
+      >
+        <View>
+          <Text style={styles.itemHourText}>{item.hour}</Text>
+          <Text style={styles.itemDurationText}>{item.duration}</Text>
+        </View>
+        <Text style={styles.itemTitleText}>{item.title}</Text>
+        <View style={styles.itemButtonContainer}>
+          <Button color={'grey'} title={'Info'} onPress={this.buttonPressed}/>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  getMarkedDates = () => {
+    const marked = {};
+    ITEMS.forEach(item => {
+      // NOTE: only mark dates with data
+      if (item.data && item.data.length > 0 && !_.isEmpty(item.data[0])) {
+        marked[item.title] = {marked: true};
+      } else {
+        marked[item.title] = {disabled: true};
+      }
+    });
+    return marked;
+  }
+
+  getTheme = () => {
+    const disabledColor = 'grey';
+
+    return {
+      // arrows
+      arrowColor: 'black',
+      arrowStyle: {padding: 0},
+      // month
+      monthTextColor: 'black',
+      textMonthFontSize: 16,
+      textMonthFontFamily: 'HelveticaNeue',
+      textMonthFontWeight: 'bold',
+      // day names
+      textSectionTitleColor: 'black',
+      textDayHeaderFontSize: 12,
+      textDayHeaderFontFamily: 'HelveticaNeue',
+      textDayHeaderFontWeight: 'normal',
+      // dates
+      dayTextColor: themeColor,
+      textDayFontSize: 18,
+      textDayFontFamily: 'HelveticaNeue',
+      textDayFontWeight: '500',
+      textDayStyle: {marginTop: Platform.OS === 'android' ? 2 : 4},
+      // selected date
+      selectedDayBackgroundColor: themeColor,
+      selectedDayTextColor: 'white',
+      // disabled date
+      textDisabledColor: disabledColor,
+      // dot (marked date)
+      dotColor: themeColor,
+      selectedDotColor: 'white',
+      disabledDotColor: disabledColor,
+      dotStyle: {marginTop: -2}
     };
   }
 
   render() {
     return (
-      <Agenda
-        
-        items={this.state.items}
-        loadItemsForMonth={this.loadItems.bind(this)}
-        selected={'2017-05-16'}
-        renderItem={this.renderItem.bind(this)}
-        renderEmptyDate={this.renderEmptyDate.bind(this)}
-        rowHasChanged={this.rowHasChanged.bind(this)}
-        // markingType={'period'}
-        // markedDates={{
-        //    '2017-05-08': {textColor: '#43515c'},
-        //    '2017-05-09': {textColor: '#43515c'},
-        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
-        //    '2017-05-21': {startingDay: true, color: 'blue'},
-        //    '2017-05-22': {endingDay: true, color: 'gray'},
-        //    '2017-05-24': {startingDay: true, color: 'gray'},
-        //    '2017-05-25': {color: 'gray'},
-        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
-        // monthFormat={'yyyy'}
-        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
-        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
-        // hideExtraDays={false}
-      />
-    );
-  }
-
-  loadItems(day) {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            this.state.items[strTime].push({
-              name: 'Item for ' + strTime + ' #' + j,
-              height: Math.max(50, Math.floor(Math.random() * 150))
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
-      this.setState({
-        items: newItems
-      });
-    }, 1000);
-  }
-
-  renderItem(item) {
-    return (
-      <TouchableOpacity
-        
-        style={[styles.item, {height: item.height}]} 
-        onPress={() => Alert.alert(item.name)}
+      <CalendarProvider
+        date={ITEMS[0].title}
+        onDateChanged={this.onDateChanged}
+        onMonthChange={this.onMonthChange}
+        showTodayButton
+        disabledOpacity={0.6}
+        // theme={{
+        //   todayButtonTextColor: themeColor
+        // }}
+        // todayBottomMargin={16}
       >
-        <Text>{item.name}</Text>
-      </TouchableOpacity>
+        {this.props.weekView ?
+          <WeekCalendar
+            
+            firstDay={1}
+            markedDates={this.getMarkedDates()}
+          /> :
+          <ExpandableCalendar
+            
+            // horizontal={false}
+            // hideArrows
+            // disablePan
+            // hideKnob
+            // initialPosition={ExpandableCalendar.positions.OPEN}
+            // calendarStyle={styles.calendar}
+            // headerStyle={styles.calendar} // for horizontal only
+            // disableWeekScroll
+            // theme={this.getTheme()}
+            disableAllTouchEventsForDisabledDays
+            firstDay={1}
+            markedDates={this.getMarkedDates()} // {'2019-06-01': {marked: true}, '2019-06-02': {marked: true}, '2019-06-03': {marked: true}};
+            //leftArrowImageSource={require('../img/previous.png')}
+            //rightArrowImageSource={require('../img/next.png')}
+          />
+        }
+        <AgendaList
+          sections={ITEMS}
+          extraData={this.state}
+          renderItem={this.renderItem}
+          // sectionStyle={styles.section}
+        />
+      </CalendarProvider>
     );
-  }
-
-  renderEmptyDate() {
-    return (
-      <View style={styles.emptyDate}>
-        <Text>This is empty date!</Text>
-      </View>
-    );
-  }
-
-  rowHasChanged(r1, r2) {
-    return r1.name !== r2.name;
-  }
-
-  timeToString(time) {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
   }
 }
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: 'white',
-    flex: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    marginTop: 17
+  calendar: {
+    paddingLeft: 20,
+    paddingRight: 20
   },
-  emptyDate: {
-    height: 15,
-    flex:1,
-    paddingTop: 30
+  section: {
+    backgroundColor: lightThemeColor,
+    color: 'grey',
+    textTransform: 'capitalize'
+  },
+  item: {
+    padding: 20,
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+    flexDirection: 'row'
+  },
+  itemHourText: {
+    color: 'black'
+  },
+  itemDurationText: {
+    color: 'grey',
+    fontSize: 12,
+    marginTop: 4,
+    marginLeft: 4
+  },
+  itemTitleText: {
+    color: 'black',
+    marginLeft: 16,
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  itemButtonContainer: {
+    flex: 1,
+    alignItems: 'flex-end'
+  },
+  emptyItem: {
+    paddingLeft: 20,
+    height: 52,
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey'
+  },
+  emptyItemText: {
+    color: 'lightgrey',
+    fontSize: 14
   }
 });
