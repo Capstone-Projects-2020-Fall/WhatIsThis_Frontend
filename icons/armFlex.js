ecbc0e54ed5065db36785b587b5f8a66146d224b
Parth Patel <tug59168@temple.edu>
2020-11-19 01:54:02 -0500

    added icons to tab

---
+++ icons/armFlex.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import SVG, {Path} from 'react-native-svg';
+
+//SVG file was used for the Google icon.
+
+const ArmFlexIcon= () => {
+  return (
+    <SVG version="1.1" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <Path d="M3 18.34C3 18.34 4 7.09 7 3L12 4L11 7.09H9V14.25H10C12 11.18 16.14 10.06 18.64 11.18C21.94 12.71 21.64 17.32 18.64 19.36C16.24 21 9 22.43 3 18.34Z" />
+    </SVG>
+
+  )
+};
+
+
+export default ArmFlexIcon;
+
