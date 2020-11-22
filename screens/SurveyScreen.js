f383c1e1cd1992b9d1a81f87f5b6f52e7ee99dfd
Mathew Anthony <tuf62412@temple.edu>
2020-11-22 13:24:58 -0500

    Different approach, uses component, "native-forms" to create forms for user feedback.

--- screens/SurveyScreen.js
+++ screens/SurveyScreen.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react';
-import {Text, View} from 'react-native';
+import React, { Component, useState } from 'react';
+import {Text, View, Button, StyleSheet} from 'react-native';
+import NativeForms from 'native-forms';
 
 class SurveyScreen extends Component {
     render() {
         return (
-            <View style={{ flex:1, justifyContent: "center", alignItems: "center"}}>
-                <Text>
-                    Hello world! 
-                </Text>
+            <View style={styles.container}>
+                    <NativeForms
+                        form="https://my.nativeforms.com/YDVslDbp1jZm4WTNBDVy0Db"
+                    />
             </View>
         );
     }
 
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center', 
+        justifyContent: 'center',
+    },
+})
 
 export default SurveyScreen;
