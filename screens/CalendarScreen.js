cc23e2b7aca5db67cb8451e14614eadb453e7d26
Parth Patel <tug59168@temple.edu>
2020-11-22 20:46:46 -0500

    addEventToCalendar

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -7,7 +7,8 @@ import {
   View,
   Text,
   TouchableOpacity,
-  Button
+  Button,
+  Image
 } from 'react-native';
 import {ExpandableCalendar, AgendaList, CalendarProvider, WeekCalendar} from 'react-native-calendars';
 
@@ -18,7 +19,7 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-
+// Original today data variable -> UTC -> EST (Philly)
 const today = new Date().toISOString().split('T')[0];
 
 const todaysDate = new Date();
@@ -38,9 +39,12 @@ console.log("UTC Offset: " + UTCOffset);
 console.log("Today in EST: " + todayEST + "\n\n");
 
 
-const fastDate = getPastDate(3);
+const pastDate = getPastDate(3);
+//console.log(pastDate)
 const futureDates = getFutureDates(9);
-const dates = [fastDate, todayEST].concat(futureDates);
+const dates = [pastDate, todayEST].concat(futureDates);
+//console.log(dates)
+
 const themeColor = '#00AAAF';
 const lightThemeColor = '#EBF9F9';
 
@@ -69,10 +73,12 @@ function getPastDate(days) {
 /**
  * The ITEMS array holds the events
 */
+// Array -> Map(title, data->Array-> Map(name))
 
 const ITEMS = [
   {title: dates[0], data: [{name: 'Bench Press'}]},
   {title: dates[1], data: [{name: 'Bent Over Row'}, {name: 'Bicep Curl with Dumbbells'}]},
+  {title: dates[1], data: [{name: 'Cycling'}, {name: 'Deadlift'}]},
   {title: dates[2], data: [{name: 'Bicep Curls with Barbell'}, {name: 'Chin Ups'}, {name: 'Crunches'}]},
   {title: dates[3], data: [{name: 'Cycling'}]},
   {title: dates[4], data: [{name: "Deadlift"}]},
@@ -84,6 +90,21 @@ const ITEMS = [
   {title: dates[9], data: [{name: 'Bench Press'}, {name: 'Bent Over Row'}, {name: 'Chin Ups'}]},
   {title: dates[10], data: [{ name: 'Deadlift'}]}
 ];
+//console.log("ITEMS ARRAY: " + ITEMS[2].title + ITEMS[2].data.forEach((element)=>{console.log(element.name);}));
+
+// store date string such as 2020-11-20
+const date = [];
+
+// ITEMS =[{title: date[0], data: [{name: 'Bench Press'}, {name: 'Deadlift'}]}]
+const items = [];
+
+/* Adding 
+
+Pressed "+" 
+[]
+
+
+*/
 
 /*
 const eventArrExercise = ITEMS.map(events => {
@@ -92,16 +113,12 @@ const eventArrExercise = ITEMS.map(events => {
 });
 console.log(eventArrExercise);
 */
-const valuesOfITEMS = ITEMS.values();
+//const valuesOfITEMS = ITEMS.values();
 
 //console.log(valuesOfITEMS.next().value.title);
 //console.log("\n");
 //console.log(valuesOfITEMS.next().value);
-//console.log("ITEMS values:\n");
-//console.log(valuesOfITEMS.next());
-for(let valueITEMS of valuesOfITEMS){
-  console.log(valueITEMS);
-}
+
 /*
 const eventArrExercise = ITEMS.map(events => {
   //console.log(events.title),
@@ -140,14 +157,14 @@ function buildArray(eventsArray){
         workoutList.push(itemEvent.data[0].name, "\n\n");
     }
   })
-  console.log(workoutList);
+  //console.log(workoutList);
   return workoutList;
   //return console.log(workoutList);
 }
 
 buildArray(ITEMS);
 
-console.log("\n\nhello world \n\n")
+
 
 //console.log(eventArrExercise);
 function retrieveEventsFromUserDatabase(){
@@ -155,6 +172,107 @@ function retrieveEventsFromUserDatabase(){
   return console.log("Retreive user information");
 }
 
+
+
+
+
+//This would have to be an array of an array of objects.
+//Have an array of objects that the events get stored when creating them 
+const exerciseArray = [
+  ["Running", "Bench Press"], 
+  ["Leg Press", "Deadlift"]
+];
+
+const dateArray = ["2020-11-29", "2020-12-01"];
+
+//{title: dates[1], data: [{name: 'Bent Over Row'}, {name: 'Bicep Curl with Dumbbells'}]}
+function addEventToArray(eventArray,date,exerciseArray) {
+  for(var i = 0; i < date.length; i++){
+    for(var j = 0; j < exerciseArray[i].length; j++){
+      eventArray.push({title: date[i], data: [{name: exerciseArray[i][j]}]});
+      
+    }
+  }
+  return eventArray;
+}
+
+function printAddEventToArray(eventArray, date, exerciseArray){
+  for(var i = 0; i < date.length; i++){
+    for(var j = 0; j < exerciseArray[i].length; j++){
+      eventArray.push({title: date[i], data: [{name: exerciseArray[i][j]}]});
+      
+    }
+  }
+  return conosle.log(eventArray);
+}
+
+printAddEventToArray(items, date, exerciseArray);
+
+const dateString = "2020-12-01";
+const exerciseName = "Deadlift";
+var removedResult;
+const resultArray = [];
+const arrayEvents = [
+  {
+    title: "2020-12-25", 
+    data: [
+      {
+        name: 'Cycling'
+      }, 
+      {
+        name: 'Deadlift'
+      }
+    ]
+  }
+  ];
+/*
+for(var i=0; i < arrayEvents.length; i++){
+  console.log("Iteratation i = " + i);
+  console.log(arrayEvents.length);
+  console.log("Length of data: " + arrayEvents[i].data.length);
+  //console.log("arrayEvents i = " + i + ":\n" + arrayEvents[i].data[1].name);
+  const result = arrayEvents[i];
+  console.log("result: " + arrayEvents[0].data[0].name);
+  for(var j=0; j < arrayEvents[i].data.length; j++){
+    //console.log(arrayEvents[i].data[j].name);
+    if(arrayEvents[i].data[j].name === "Deadlift"){
+      console.log("Iterate j");
+      //resultArray = arrayEvents[i].data[j].name
+      //arrayEvents.pop(arrayEvents[i].data[j].name);
+      console.log("Name of exercise: " + arrayEvents[i].data[j].name);
+      //console.log()
+      //break;
+    }
+  }
+}*/
+
+
+//console.log(arrayEvents[0].data[]);
+/*for(var i=0; i < arrayEvents.length; i++){
+  arrayEvents.pop(arrayEvents[i].data);
+  
+}*/
+console.log(JSON.stringify(arrayEvents))
+//console.log(arrayEvents.length);
+//console.log("result array:");
+//console.log(resultArray);
+console.log("ARRAY ARRAY ARRAY");
+
+// Iterate through the array of events and pop of the exercise e.g. {name: "Running"}
+function removeEventFromArray(eventArray,dateString,exerciseName){
+  for(var i = 0; i < eventArray.length; i++){
+    for(var j = 0; j < eventArray[i].data.length; j++){
+      if (eventArray[i].data[j].name === exerciseName) {
+        conosle.log(eventArray[i].data[j].name);
+        break;
+      }
+    }
+  }
+  
+  return console.log(eventArray[i].data[j].name);
+}
+
+//items = addEventToArray(items, dateArray, exerciseArray);
 
 function addEventsToFirestore(eventsArray){
   //const user =  firebase.auth().currentUser;
@@ -191,7 +309,7 @@ function removeEventsFromFirestore(eventsArray) {
       console.log("User is signed in.\n");
       console.log("Current User ID: " + user.uid);
       firestore().collection('user').doc(user.uid).update({
-        workoutEvents: firebase.firestore.FieldValue.arrayRemove("2020-11-15||Bench Press")});
+        workoutEvents: firebase.firestore.FieldValue.arrayRemove("2020-11-15||Leg Press")});
     } else {
       // No user is signed in.
       console.log("No user is signed in.\n");
@@ -244,9 +362,8 @@ export default class ExpandableCalendarScreen extends Component {
       <TouchableOpacity
         onPress={() => this.itemPressed(item.name)}
         style={styles.item}
-        
-      >
-      
+        /*ITEMS = [{title: date}, data:{name: "Running"}] */
+      > 
     <Text style={styles.itemTitleText}>{item.name}</Text>
         <View style={styles.itemButtonContainer}>
           <Button color={'grey'} title={'Info'} onPress={this.buttonPressed}/>
@@ -257,7 +374,7 @@ export default class ExpandableCalendarScreen extends Component {
 
   getMarkedDates = () => {
     const marked = {};
-    ITEMS.forEach(item => {
+    items.forEach(item => {
       // NOTE: only mark dates with data
       if (item.data && item.data.length > 0 && !_.isEmpty(item.data[0])) {
         marked[item.title] = {marked: true};
@@ -269,6 +386,10 @@ export default class ExpandableCalendarScreen extends Component {
     });
     return marked;
   }
+  
+  items = addEventToArray(items, dateArray, exerciseArray); 
+  //result = removeEventFromArray(items, dateString, exerciseName);
+
 
   getTheme = () => {
     const disabledColor = 'grey';
@@ -309,7 +430,7 @@ export default class ExpandableCalendarScreen extends Component {
   render() {
     return (
       <CalendarProvider
-        date={ITEMS[0].title}
+        date={items[0].title}
         onDateChanged={this.onDateChanged}
         onMonthChange={this.onMonthChange}
         showTodayButton
@@ -345,11 +466,30 @@ export default class ExpandableCalendarScreen extends Component {
           />
         }
         <AgendaList
-          sections={ITEMS}
+          sections={items}
           extraData={this.state}
           renderItem={this.renderItem}
           // sectionStyle={styles.section}
         />
+        <TouchableOpacity
+          onPress={() =>
+            /*navigation.navigate('CreateTask', {
+            updateCurrentTask: this._updateCurrentTask,
+            currentDate,
+            createNewCalendar: this._createNewCalendar,
+            })*/
+            Alert.alert('Adding events button')
+          }
+          style={styles.viewTask}
+        >
+          <Image
+            source={require('../assets/plus.png')}
+            style={{
+              height: 30,
+              width: 30,
+            }}
+          />
+        </TouchableOpacity>
       </CalendarProvider>
     );
   }
@@ -401,5 +541,27 @@ const styles = StyleSheet.create({
   emptyItemText: {
     color: 'lightgrey',
     fontSize: 14
+  },
+  viewTask: {
+    position: 'absolute',
+    bottom: 40,
+    right: 17,
+    height: 60,
+    width: 60,
+    //backgroundColor: '#2E66E7',
+    backgroundColor: '#00acee',
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    //shadowColor: '#2E66E7',
+    shadowColor: '#00acee',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowRadius: 30,
+    shadowOpacity: 0.5,
+    elevation: 5,
+    zIndex: 999,
   }
 });
