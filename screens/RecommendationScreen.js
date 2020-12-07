df420391590e532cceb400e242960e2f46513b37
Mark Heimann <tuj52064@temple.edu>
2020-12-06 19:18:15 -0500

    updated display

--- screens/RecommendationScreen.js
+++ screens/RecommendationScreen.js
@@ -9,9 +9,13 @@ import {
   TouchableOpacity,
   Button,
   Image,
+  Modal,
+  ScrollView,
+  TouchableWithoutFeedback
 } from 'react-native';
 import firebase, {firestore} from 'firebase';
 import {firebaseConfig} from '../config';
+import ExerciseModal from '../ExerciseModal';
 
 class RecommendationScreen extends Component {	
 	constructor(props){
@@ -20,7 +24,7 @@ class RecommendationScreen extends Component {
 		this.state = {
 			exercises: [],
 			recentWorkouts: [],
-			isVisible : false,
+			isVisible : true,
 			formalName: "blank"
 		};
 	}
@@ -46,6 +50,11 @@ class RecommendationScreen extends Component {
 						const data = querySnapshot.docs.map(doc => doc.data());
 						this.setState({exercises: data});
 					});		
+	}
+	
+	displayModal(){
+		const {isVisible} = this.state
+		this.setState({isVisible: !isVisible})
 	}
 
     render() {	
@@ -73,24 +82,99 @@ class RecommendationScreen extends Component {
 			
 			return unusedExercises[Math.floor(Math.random()*unusedExercises.length)];
 		}
+		
+		function returnExerciseInfo(){
+			const chosenExercise = returnUnusedExercise();
+			exercises.forEach(exercise =>{
+				if(exercise.name = chosenExercise) return exercise;
+			});
+		}
 
         return (
-			<View>
-				<Text>
-					{returnUnusedExercise()}
-				</Text>
+			<View style={{ flex: 1 }}>
+				<Modal
+				  animationType = {"slide"}
+				  transparent = {true}
+				  visible = {this.state.isVisible}
+				  onRequestClose = {() => {
+					{this.displayModal()}
+				  }}
+				>
+
+					<TouchableOpacity
+					  style = {styles.modalContainer}
+					  activeOpacity= {1}
+					  onPressOut={() => {this.displayModal()}}
+					>
+					  <TouchableWithoutFeedback>
+						<View style ={styles.modalView}>
+						  <ExerciseModal  name={"Bench Press"} description={"Test"} image={"https://firebasestorage.googleapis.com/v0/b/fir-react-native-b2fff.appspot.com/o/diagrams%2Fbench.png?alt=media&token=22438c6f-7e1b-40bf-83db-5a43479f1036"}/>
+						  
+						 
+						  <TouchableOpacity style={styles.button} onPress={() => {
+							  this.displayModal()
+							}}>
+							  <Text style={styles.buttonText}>Close</Text>
+						  </TouchableOpacity>
+						</View>
+					  </TouchableWithoutFeedback>
+					</TouchableOpacity>
+
+				</Modal>
 			</View>  
         );
     }
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center', 
-        justifyContent: 'center',
-    },
-});
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  modalContainer: {
+    padding: 25,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cameraArea : {
+    flex: 1,
+    margin: 10
+  },
+  modalView: {
+    margin: 5,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    }
+  },
+  modalText: {
+    fontSize: 14,
+    marginBottom: 10,
+    padding: 1,
+  },
+  image: {
+    marginTop: 150,
+    marginBottom: 10,
+    width: '100%',
+    height: 350,
+  },
+  button: {
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    backgroundColor: '#00bfff',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 20
+  }
+})
 
 export default RecommendationScreen;
